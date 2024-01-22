295444749d1c59bf73423721dfa3a1c814100311
Divya <g.priscilladivya@gmail.com>
2024-01-22 21:08:18 +0530

    22-01-24

--- task 5- JS/script.js
+++ task 5- JS/script.js
@@ -36,30 +36,29 @@ for(n=1; n<=num; n++){
     console.log(`${i} is a prime number`);
  }   
 // Getting inputs from user and displaying 
-let start = prompt('Hello, Enter the starting range');
-let end = prompt('Enter the ending range');
-console.log(`starting range:- ${start}`);
-console.log(`Ending range:- ${end}`);
-if(start && end != null){
-    console.log('values are collected from user');
-    for (i = start; start <= end; start++) {
-        console.log(start);   
-    }
-} else{
-    console.log('Values are null');
-}
+// let start = prompt('Hello, Enter the starting range');
+// let end = prompt('Enter the ending range');
+// console.log(`starting range:- ${start}`);
+// console.log(`Ending range:- ${end}`);
+// if(start && end != null){
+//     console.log('values are collected from user');
+//     for (i = start; start <= end; start++) {
+//         console.log(start);   
+//     }
+// } else{
+//     console.log('Values are null');
+// }
 
 //Random Color Generator
 const char = '0A1B2C3D45E67F89';
-let randomColor = "#";
-for (let index = 1; index <= 6; index++) {
+    let randomColor = "#";
+    for (let index = 1; index <= 6; index++) {
     const randColor = Math.floor(Math.random() * char.length);
     randomColor += char.charAt(randColor);
-    console.log(randomColor);  
-}
+    console.log(randomColor);
+    }
 
-
-//adding para text using DOM
+// adding para text using DOM
 // window.onload = () =>{
 //     const subHeading = document.createElement("p");
 //     subHeading.innerText = "Let's understand the purpose of DOM and JS";
@@ -72,3 +71,27 @@ for (let index = 1; index <= 6; index++) {
 //     alert('Welcome');
 // });
 // }
+
+
+function genRandomColor(){
+    const char = '0A1B2C3D45E67F89';
+    let randomColor = "#";
+    for (let index = 1; index <= 6; index++) {
+    const randColor = Math.floor(Math.random() * char.length);
+    randomColor += char.charAt(randColor);
+    console.log(randomColor);
+    document.body.style.backgroundColor = randomColor;
+    const colorCode = document.querySelector("h2");
+    colorCode.innerHTML = `Your Color Code is ${randomColor}`;
+    }
+    // return randomColor;
+}
+
+const generateBtn =  document.querySelector("button");
+generateBtn.addEventListener('click', genRandomColor);
+
+
+
+
+
+
